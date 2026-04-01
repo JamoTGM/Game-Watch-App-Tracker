256f74cd86caf283b269588f9e7d057ba54568f6
Jito <James.warner24@bathspa.ac.uk>
2026-04-01 15:52:28 +0100

    Added A button to selct the game
    Can selct one game for the moment

--- Game Watch/Assets/ItemPopupController.cs
+++ Game Watch/Assets/ItemPopupController.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
 
-public class ItemPopupController
+public class ItemPopupController : MonoBehaviour
 {
-    
+    public GameObject popupPanel;
+
+    public void ShowPopup()
+    {
+        popupPanel.SetActive(true);
+    }
+
+    public void HidePopup()
+    {
+        popupPanel.SetActive(false);
+    }
 }
